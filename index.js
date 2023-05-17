9f53ccda36afddf0791d1da22e7b5b0404efe049
Shimul Das <shimul.191002110@gmail.com>
2023-05-17 21:43:46 +0600

    user authorization is overall done

--- index.js
+++ index.js
@@ -29,6 +29,24 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyjwt=(req,res,next)=>{
+  console.log("Hitting verify JWT Token");
+  console.log(req.headers.authorization)
+  const authorization=req.headers.authorization;
+  if(!req.headers.authorization){
+    return res.status(401).send({error:true,message:"Unauthorized Access"})
+  }
+  const token=authorization.split(' ')[1];
+  console.log("Inside the token",token)
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
+    if(error){
+      return res.status(403).send({error:true,message:"Unauthorized Access"})
+    }
+    req.decoded=decoded;
+    next();
+  })
+
+}
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -43,7 +61,7 @@ async function run() {
     })
     //booking or orders
     app.post('/jwt',async(req,res)=>{
-      const user=req.body;
+      const user=req.headers;
       console.log(user);
       const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
         expiresIn:'1h'
@@ -54,8 +72,15 @@ async function run() {
 
 
   //find some specific data using query
-  app.get('/booked',async(req,res)=>{
+  app.get('/booked',verifyjwt,async(req,res)=>{
+    const decoded=req.decoded;
+    console.log(decoded.email)
+    console.log("After cameback",decoded)
+    // if(decoded.email !== req.query.email){
+    //   return res.status(403).send({error:true, message:"For bidden Access"})
+    // }
     // console.log(req.query)
+    console.log(req.headers)
     let query={}
     if(req.query?.email){
       query={email:req.query.email}
